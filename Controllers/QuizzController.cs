fc58f8ad36a9264fd6ecaa7e778abd030f7e7bec
toka-yasser <t.yasser@studio50s.com>
2021-07-22 09:57:48 -0700

    First commit

---
+++ Controllers/QuizzController.cs
@@ -0,0 +1,128 @@
+﻿using QuizTest.Models;
+using QuizTest.viewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuizTest.Controllers
+{
+    public class QuizzController : Controller
+    {
+        public MultipleChoiceDbEntities1 dbContext = new MultipleChoiceDbEntities1();
+
+        [HttpGet]
+        public ActionResult GetUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetUser(UserVM user)
+        {
+            UserVM userConnected = dbContext.User.Where(u => u.FullName == user.FullName)
+                                         .Select(u => new UserVM
+                                         {
+                                             UserID = u.UserID,
+                                             FullName = u.FullName,
+                                             ProfilImage = u.ProfilImage,
+
+                                         }).FirstOrDefault();
+
+            if (userConnected != null)
+            {
+                Session["UserConnected"] = userConnected;
+                return RedirectToAction("SelectQuizz");
+            }
+            else
+            {
+                ViewBag.Msg = "Sorry : user is not found !!";
+                return View();
+            }
+
+        }
+
+        [HttpGet]
+        public ActionResult SelectQuizz()
+        {
+            QuizVM quiz = new viewModels.QuizVM();
+            quiz.ListOfQuizz = dbContext.Quiz.Select(q => new SelectListItem
+            {
+                Text = q.QuizName,
+                Value = q.QuizID.ToString()
+
+            }).ToList();
+
+            return View(quiz);
+        }
+
+        [HttpPost]
+        public ActionResult SelectQuizz(QuizVM quiz)
+        {
+            QuizVM quizSelected = dbContext.Quiz.Where(q => q.QuizID == quiz.QuizID).Select(q => new QuizVM
+            {
+                QuizID = q.QuizID,
+                QuizName = q.QuizName,
+
+            }).FirstOrDefault();
+
+            if (quizSelected != null)
+            {
+                Session["SelectedQuiz"] = quizSelected;
+
+                return RedirectToAction("QuizTest");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult QuizTest()
+        {
+            QuizVM quizSelected = Session["SelectedQuiz"] as QuizVM;
+            IQueryable<QuestionVM> questions = null;
+
+            if (quizSelected != null)
+            {
+                questions = dbContext.Question.Where(q => q.Quiz.QuizID == quizSelected.QuizID)
+                   .Select(q => new QuestionVM
+                   {
+                       QuestionID = q.QuestionID,
+                       QuestionText = q.QuestionText,
+                       Choices = q.Choice.Select(c => new ChoiceVM
+                       {
+                           ChoiceID = c.ChoiceID,
+                           ChoiceText = c.ChoiceText
+                       }).ToList()
+
+                   }).AsQueryable();
+
+
+            }
+
+            return View(questions);
+        }
+
+        [HttpPost]
+        public ActionResult QuizTest(List<QuizAnswersVM> resultQuiz)
+        {
+            List<QuizAnswersVM> finalResultQuiz = new List<viewModels.QuizAnswersVM>();
+
+            foreach (QuizAnswersVM answser in resultQuiz)
+            {
+                QuizAnswersVM result = dbContext.Answer.Where(a => a.QuestionID == answser.QuestionID).Select(a => new QuizAnswersVM
+                {
+                    QuestionID = a.QuestionID.Value,
+                    AnswerQ = a.AnswerText,
+                    isCorrect = (answser.AnswerQ.ToLower().Equals(a.AnswerText.ToLower()))
+
+                }).FirstOrDefault();
+
+                finalResultQuiz.Add(result);
+            }
+
+            return Json(new { result = finalResultQuiz }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuizTest.Startup))]
+namespace QuizTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
